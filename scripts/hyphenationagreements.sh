76067b16905663ccaf4f9f6b0f29ce262dece812
leolca <leolca@gmail.com>
2023-08-02 14:02:51 -0300

    fix hyphagreements

--- scripts/hyphenationagreements.sh
+++ scripts/hyphenationagreements.sh
@@ -1,45 +1,31 @@
 #!/bin/bash
 
 # https://unix.stackexchange.com/questions/609866/regular-awk-easily-sort-array-indexes-to-output-them-in-the-chosen-order
-cat ../data/hyphenations.csv | 
-awk -F "," '{
-   delete a; delete xa; delete idxs;
-   for(i=2; i<=NF; i++) if($i) a[$i]+=1;
-   #for(aa in sa) printf "%s(%d)\t",aa,a[aa]; if(length(a)>0) print ""
+tail -n +2 ../data/hyphenations.csv | 
+awk '
+   BEGIN {FS=OFS=","}
+   {
+   delete hyph_list; delete n_tuple;  
+   for(i=2; i<=NF; i++) if($i) hyph_list[$i]+=1;
+   #for(h in hyph_list) printf "%s(%d)\t",h,hyph_list[h]; if(length(h)>0) print ""
 
-   for(aa in a) xa[a[aa]]=aa;
+   #for(aa in a) xa[a[aa]]=aa;
    #for(x in xa) printf "%s :: %d ",xa[x],x;
-   
-   n = sort(xa,idxs,"-r")    
-   for (i=1; i<=n; i++) {
-        idx = idxs[i]
-	printf "%s(%d)%s", xa[idx], idx, (i<n ? OFS : ORS)
+   c = 0;
+   for(h in hyph_list) {
+       c++;
+       n_tuple[c] = "(" hyph_list[h] "," h ")"
+   }
+   n = length(n_tuple);
+   asort(n_tuple)
+   #for (i = 1; i <= n; i++) print n_tuple[i];
+   for (i = n; i >= 1; i--) {
+       gsub(/[\(\)]/, "", n_tuple[i])
+       split(n_tuple[i], parts, ",")
+       #print parts[2] "," parts[1]
+       printf "%s(%d)%s", parts[2], parts[1], (i>1 ? OFS : ORS)
    }
 
    } 
- 
-   function sort(arr, idxs, args,      i, str, cmd) {
-      for (i in arr) {
-         gsub(/\047/, "\047\\\047\047", i)
-         str = str i ORS
-      }
-
-      cmd = "printf \047%s\047 \047" str "\047 |sort " args
-
-      i = 0
-      while ( (cmd | getline idx) > 0 ) {
-         idxs[++i] = idx
-      }
-
-      close(cmd)
-
-      return i
-   }    
-   ' |
-   if [ $# -eq 0 ]
-   then
-       tee /dev/null
-   else
-       cut -d' ' -f1 | grep "($1)" | wc -l
-   fi
+   ' > ../data/hyphagreements
 
