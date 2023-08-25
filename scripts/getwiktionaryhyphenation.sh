a305482076019d2a5e313bf57a4adf3257bc4a65
leolca <leolca@gmail.com>
2023-08-25 18:31:24 -0300

    accept apparent proparoxytone

--- scripts/getwiktionaryhyphenation.sh
+++ scripts/getwiktionaryhyphenation.sh
@@ -3,8 +3,12 @@
 word=$1
 word4url=$(echo $word | perl -p -e 's/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg' | sed 's/%0A$//g')
 curlresult=$(curl -s "https://pt.wiktionary.org/w/api.php?action=parse&page=$word4url&prop=wikitext&format=json" | ascii2uni -a U -q)
-wordhyph=$(echo $curlresult | grep -Po "{(proparoxítona|paroxítona|oxítona)[^}]+}" | head -n1 | tr -d {} | sed -e 's/proparoxítona//g' -e 's/paroxítona//g' -e 's/oxítona//g' -e's/^|//g' -e 's/lang=[a-z]\+//g' -e 's/|$//g' | tr '|' '-')
+#echo $curlresult | grep -Po "{(proparoxítona|proparoxítona aparente|paroxítona|oxítona)[^}]+}"
+wordhyph=$(echo $curlresult | grep -Po "{(proparoxítona|proparoxítona aparente|paroxítona|oxítona)[^}]+}" | head -n1 | tr -d {} | sed -e 's/proparoxítona aparente//g' -e 's/proparoxítona//g' -e 's/paroxítona//g' -e 's/oxítona//g' -e's/^|//g' -e 's/lang=[a-z]\+//g' -e 's/|$//g' | tr '|' '-')
 wordrebuilt=$(echo $wordhyph | sed 's/-//g')
+if grep -Pq "{(proparoxítona aparente)[^}]+}" <(echo $curlresult); then
+    wordhyph="${wordhyph%-*}:${wordhyph##*-}"
+fi
 if [[ "$word" == "$wordrebuilt" ]]; then
    echo -e "$wordhyph" 
 fi
